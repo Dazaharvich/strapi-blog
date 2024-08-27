88720991e8379ccbb6bee9ac4829b010608c42b1
David Azaharvich <d.azaharvichp@gmail.com>
2024-08-27 01:02:20 -0400

    second commit

---
+++ src/components/Category.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from "react";
+
+const Category = () => {
+  return (
+    <div className="bg-sky-600 p-4 rounded-lg shadow-md cursor-pointer" onClick={() => {}}>
+      Title
+    </div>
+  );
+};
+
+export default Category;
